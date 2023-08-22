6b6fb9a9bc134bd13a6bf4e49dec79110dfe6a48
ysdko <koki.yoshida110@gmail.com>
2023-08-23 03:50:28 +0900

    add:uuidでルームを分ける

--- src/RoomCreationScreen.tsx
+++ src/RoomCreationScreen.tsx
@@ -1,29 +1,26 @@
 // RoomCreationScreen.tsx
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom'; // Note the useNavigate import
 
+import { v4 as uuidv4 } from 'uuid';  // uuidパッケージからv4関数をインポート
+
 const RoomCreationScreen: React.FC = () => {
-  const navigate = useNavigate(); // Use useNavigate hook instead of useHistory
-  const [roomName, setRoomName] = useState<string>('');
+  const navigate = useNavigate();
 
   const createRoom = () => {
-    // Send roomName to the backend to create a new room
-    // Once the room is created, navigate to the room page
-    navigate(`/room/${roomName}`); // Use navigate function
+    const roomUUID = uuidv4();  // UUIDを生成
+
+    // UUIDをバックエンドに送信して新しい部屋を作成するロジック（省略）
+
+    navigate(`/room/${roomUUID}`);  // UUIDを用いて部屋に遷移
   };
 
   return (
     <div>
       <h1>Create a Room</h1>
-      <input
-        value={roomName}
-        onChange={(e) => setRoomName(e.target.value)}
-        placeholder="Enter room name"
-      />
       <button onClick={createRoom}>Create Room</button>
     </div>
   );
 };
 
 export default RoomCreationScreen;
-
